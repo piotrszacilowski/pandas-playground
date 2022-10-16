70f2730416cb6da5d7b06a278fba5d5691fdeacd
Piotr Szacilowski <szacilowski.piotr@gmail.com>
2022-10-16 20:19:53 +0200

    update structure

---
+++ src/electricity_price.py
@@ -0,0 +1,34 @@
+import typing as t
+
+import requests as r
+from bs4 import BeautifulSoup
+
+from exceptions import ApiIsUnreachable, DataFetchingFailed
+
+URL: str = 'https://www.globalpetrolprices.com/Poland/electricity_prices/'
+
+
+def fetch_data() -> t.Dict:
+    resp = r.get(url=URL)
+
+    if resp.status_code != 200:
+        raise ApiIsUnreachable()
+
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+    table = soup.find_all('table')[0]
+    scraped_data = []
+    for table_data in table.find_all('tr'):
+        for data in table_data.find_all('td'):
+            scraped_data.append(data.text.strip())
+
+    try:
+        price = float(scraped_data[2])
+        unit = scraped_data[1][-3:]
+    except ValueError:
+        raise DataFetchingFailed()
+    return {'value': price, 'unit': unit, 'currency': 'PLN'}
+
+
+def get_electricity_price_mock() -> t.Dict:
+    return {'value': 0.790, 'unit': 'kWh', 'currency': 'PLN'}
---
+++ src/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from electricity_price import fetch_data
+
+# spreadsheet = pd.read_csv("../data/photovoltaics-2021.xls", engine='python')
+price = fetch_data()
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+print(df)
+print('-' * 9)
+print(df['Age'].max())
+print(df['Age'][0])
+print(type(df['Age']))
+
